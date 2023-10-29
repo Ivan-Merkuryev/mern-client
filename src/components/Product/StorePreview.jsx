d9a79457c24775d47e7d2d3600fea894a155a764
Ivan-Merkuryev <129294036+Ivan-Merkuryev@users.noreply.github.com>
2023-10-29 20:28:09 +0300

    Update StorePreview.jsx

--- src/components/Product/StorePreview.jsx
+++ src/components/Product/StorePreview.jsx
@@ -14,13 +14,13 @@ function StorePreview({
       <div className="carts-cart">
         <Link to={`/members/${member.address}`}>
           <div className="member-store">
-            <img className="cart-avatar" src={`http://localhost:4080${member.info.avatar}`} alt="" />
+            <img className="cart-avatar" src={`https://mern-art-app.onrender.com${member.info.avatar}`} alt="" />
             <p> {member.info.nameGroup} </p>
           </div>
         </Link>
         <Link to={`/store/${id}`} class="link">
           <div className="img">
-            <img  src={`http://localhost:4080${productUrl[0]}`} alt="" className="carts-cart_img" />
+            <img  src={`https://mern-art-app.onrender.com${productUrl[0]}`} alt="" className="carts-cart_img" />
           </div>
           <p className="carts-cart_title">{title}</p>
           <span className="carts-cart_text">{description}</span>
