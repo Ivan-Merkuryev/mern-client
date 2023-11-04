a06817b969befa80ac60d458e4c1ed359d778015
Ivan-Merkuryev <129294036+Ivan-Merkuryev@users.noreply.github.com>
2023-11-04 16:22:06 +0300

    Update Author.jsx

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -4,7 +4,7 @@ function Author({ name, logo, address }) {
   return (
     <div style={{display: 'flex', alignContent: 'flex-start'}}>
       <Link to={`/members/${address}`} className="author">
-        <img src={`http://localhost:4080${logo}`} alt="" className="logo" />
+        <img src={`https://mern-art-app.onrender.com${logo}`} alt="" className="logo" />
         <p>{name}</p>
       </Link>
     </div>
