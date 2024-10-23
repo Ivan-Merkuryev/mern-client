0d2bb95da242e6a8406871a7c30bcebab034caff
Ivan-Merkuryev <129294036+Ivan-Merkuryev@users.noreply.github.com>
2024-10-23 11:18:37 +0300

    Update Post.jsx

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,107 +1,39 @@
 import { Link } from "react-router-dom";
 import "./Post.sass";
-
 import * as React from "react";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
-import { Avatar } from "@mui/material";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { styled } from "@mui/material/styles";
-
-const theme = createTheme({
-  typography: {
-    fontFamily: "Montserrat, sans-serif", // замените шрифт на нужный вам
-  },
-});
-
-const CartMedia = styled(CardContent)(`
-  width: 245px;
-  height: 150px;
-  object-fit: cover;
-  padding: 0;
-  border-radius: 12px;
-  overflow-x: visible;
-`);
+import marking from '../marking'
+import trimText from '../trimText'
 
 function Post({ id, title, text, member, previewImg }) {
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <Card sx={{ width: 520, borderRadius: "16px", paddingBottom: 0 }}>
-          <CardActionArea sx={{ borderRadius: 0 }}>
-            <Link to={`/members/${member.address}`} className="min-avatar">
-              <Avatar
-                alt={member.info.nameGroup}
-                src={`https://mern-art-app.onrender.com${member.info.avatar}`}
-                sx={{ width: 60, height: 60, mr: 1.5 }}
-              />
-              <span>{member.info.nameGroup}</span>
-            </Link>
-          </CardActionArea>
-          <CardActionArea sx={{ borderRadius: 0 }}>
-            <Link to={`/posts/${id}`}>
-              <CardContent
-                sx={{
-                  display: "grid",
-                  gridTemplateColumns: "1fr 1fr",
-                  gap: "2%",
-                  paddingTop: 0,
-                  height: 174,
-                  boxSizing: 'content-box',
-                  overflowY: 'visible',
-                  "&:last-child": {
-                    paddingBottom: 0,
-                  },
-                }}
-              >
-                <CartMedia
-                  component="img"
-                  src={`https://mern-art-app.onrender.com${previewImg}`}
-                  // alt="green iguana"
-                />
-                <CardContent
-                  sx={{
-                    width: 245,
-                    padding: 0,
-                    height: 140,
-                    "&:last-child": {
-                      paddingBottom: 0,
-                    },
-                  }}
-                >
-                  <Typography
-                    gutterBottom
-                    variant="h5"
-                    component="div"
-                    sx={{
-                      whiteSpace: "nowrap",
-                      overflow: "hidden",
-                      textOverflow: "ellipsis",
-                    }}
-                  >
-                    {title}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    sx={{
-                      display: "-webkit-box",
-                      WebkitLineClamp: 4,
-                      WebkitBoxOrient: "vertical",
-                      overflow: "hidden",
-                      textOverflow: "ellipsis",
-                    }}
-                  >
-                    {text}
-                  </Typography>
-                </CardContent>
-              </CardContent>
-            </Link>
-          </CardActionArea>
-        </Card>
-      </ThemeProvider>
-    </>
+    <div className="post">
+        <Link to={`/members/${member.info.address}`} className="post-author">
+          <img
+            src={`https://mern-art-app.onrender.com${member.info.avatar}`}
+            alt=""
+            className="ava"
+          />
+          <p>{member.info.nameGroup}</p>
+        </Link>
+
+      <div className="post-content">
+        <Link to={`/posts/${id}`}>
+          <div className="image-wrapper">
+            <img
+              src={`https://mern-art-app.onrender.com${previewImg}`}
+              alt=""
+              className="image"
+            />
+          </div>
+        </Link>
+        <Link to={`/posts/${id}`}>
+          <div className="post-right">
+            <p className="bold">{title}</p>
+            <p className="m-top">{trimText(marking(text))}</p>
+          </div>
+        </Link>
+      </div>
+    </div>
   );
 }
 
