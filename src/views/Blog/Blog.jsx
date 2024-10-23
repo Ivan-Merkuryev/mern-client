bab206fcd12c32ac785286a68117a922caeb2b55
Ivan-Merkuryev <129294036+Ivan-Merkuryev@users.noreply.github.com>
2024-10-23 11:17:32 +0300

    Update Blog.jsx

--- src/views/Blog/Blog.jsx
+++ src/views/Blog/Blog.jsx
@@ -5,12 +5,7 @@ import { fetchPosts } from "../../redux/slices/posts";
 import Header from "../../components/Header/Header";
 import Post from "../../components/Post/Post";
 import "./Blog.sass";
-
-import Skeleton from "@mui/material/Skeleton";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
-
+import Loader from '../../components/Loader'
 function Blog() {
   const dispatch = useDispatch();
 
@@ -20,111 +15,28 @@ function Blog() {
     dispatch(fetchPosts());
   }, []);
 
-  const postsStatus = posts.status;
-
+  if (posts.status === "loading") {
+    return (
+      <>
+        <Header />
+        <Loader err={posts.status === "error"} />
+      </>
+    );
+  }
   return (
     <>
       <Header />
       <h2 className="h2-page">Блог</h2>
-      <div className="posts">
-        {postsStatus !== "loaded" ? (
-          <>
-            {Array.from({ length: 4 }).map((_, index) => (
-              <>
-                <Card sx={{ width: 520, borderRadius: "16px" }}>
-                  <CardContent
-                    sx={{
-                      padding: 0,
-                      m: "10px",
-                      display: "flex",
-                      alignItems: "center",
-                    }}
-                  >
-                    <Skeleton
-                      variant="circular"
-                      sx={{ width: 60, height: 60, mr: 1.5 }}
-                    />
-                    <Skeleton variant="text" width={190} height={30} />
-                  </CardContent>
-                  <CardContent
-                    sx={{
-                      display: "grid",
-                      gridTemplateColumns: "1fr 1fr",
-                      gap: "2%",
-                      paddingTop: 0,
-                      paddingBottom: "24px",
-                    }}
-                  >
-                    <Skeleton
-                      variant="rectangular"
-                      sx={{
-                        width: 245,
-                        height: 150,
-                        pt: 0,
-                        pb: 0,
-                        borderRadius: "12px",
-                      }}
-                    />
-                  </CardContent>
-                </Card>
-              </>
-            ))}
-          </>
-        ) : (
-          <>
-            {posts.items.map((obj, index) => (
-              <Post
-                key={obj._id}
-                id={obj._id}
-                title={obj.title}
-                text={obj.text}
-                member={obj.member}
-                previewImg={obj.previewImg}
-              />
-            ))}
-          </>
-        )}
-      </div>
-      <div className="posts">
-        {Array.from({ length: 4 }).map((_, index) => (
-          <>
-            <Card sx={{ width: 520, borderRadius: "16px" }}>
-              <CardContent
-                sx={{
-                  padding: 0,
-                  m: "10px",
-                  display: "flex",
-                  alignItems: "center",
-                }}
-              >
-                <Skeleton
-                  variant="circular"
-                  sx={{ width: 60, height: 60, mr: 1.5 }}
-                />
-                <Skeleton variant="text" width={190} height={30} />
-              </CardContent>
-              <CardContent
-                sx={{
-                  display: "grid",
-                  gridTemplateColumns: "1fr 1fr",
-                  gap: "2%",
-                  paddingTop: 0,
-                  // paddingBottom: "24px",
-                }}
-              >
-                <Skeleton
-                  variant="rectangular"
-                  sx={{
-                    width: 245,
-                    height: 150,
-                    pt: 0,
-                    pb: 0,
-                    borderRadius: "12px",
-                  }}
-                />
-              </CardContent>
-            </Card>
-          </>
+      <div className="blog-list">
+        {posts.items.map((obj, index) => (
+          <Post
+            key={obj._id}
+            id={obj._id}
+            title={obj.title}
+            text={obj.text}
+            member={obj.member}
+            previewImg={obj.previewImg}
+          />
         ))}
       </div>
     </>
